6eb30d9fb305263e1144762b433ef6fdad97dd45
Dimitrije Markovic <126205228+Dimitrije108@users.noreply.github.com>
2023-09-27 14:20:33 +0200

    Add game function
    Add a function to play, and keep score of, a 5 round game of rock, paper, scissors vs computer

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const computerSelection = getComputerChoice();
-const input = prompt("Type in your choice: Rock, Paper or Scissors");
-const playerSelection = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+const computerSelection = getComputerChoice;
+const playerSelection = getPlayerChoice;
+
+
+function getPlayerChoice() {
+    let input = prompt("Type in your choice: Rock, Paper or Scissors, to play a 5 round game vs computer");
+    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+}
 
 
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         return "Rock";
-    }
-    else if (randomNumber === 1) {
+    } else if (randomNumber === 1) {
         return "Paper";
-    }
-    else {
+    } else {
         return "Scissors";
     }
 }
@@ -19,31 +22,60 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Rock") {
-        return alert("Draw! You've both selected Rock");
+        return "Draw! You've both selected Rock";
     }   if (playerSelection === "Paper" && computerSelection === "Rock") {
-            return alert("You Win! Paper beats Rock");
+            return "You Win! Paper beats Rock";
         }       if (playerSelection === "Scissors" && computerSelection === "Rock") {
-                        return alert("You Lose! Rock beats Scissors");
+                    return "You Lose! Rock beats Scissors";
                 }   else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-                        return alert("You Win! Scissors beat Paper");
+                    return "You Win! Scissors beat Paper";
                 }   else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-                        return alert("Draw! You've both selected Scissors");
+                    return "Draw! You've both selected Scissors";
         } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-                return alert("Draw! You've both selected Paper");
-        }   else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-                return alert("You Lose! Scissors beat Paper");
+            return "Draw! You've both selected Paper";
+        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            return "You Lose! Scissors beat Paper";
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return alert("You Lose! Paper beats Rock");
+        return "You Lose! Paper beats Rock";
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return alert("You Win! Rock beats Scissors");
+        return "You Win! Rock beats Scissors";
     } else {
-        return alert("Type in Rock, Paper or Scissors");
+        return "Type in Rock, Paper or Scissors";
     }
     }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let round = 0; round < 5; round++) {
+        let roundResult = playRound(getPlayerChoice(), getComputerChoice());
+        if (roundResult.startsWith("You Win!") === true) {
+            playerScore += 1;
+            alert(roundResult);
+        } else if (roundResult.startsWith("You Lose!") === true) {
+            computerScore += 1;
+            alert(roundResult);
+        } else {
+            alert(roundResult);
+            continue;
+        }
+
+        console.log(playerScore);
+        console.log(computerScore);
+        console.log(roundResult);
+    }
+
+    if (playerScore > computerScore) {
+        return alert (`You Win! ${playerScore} to ${computerScore}`);
+    } else if (playerScore < computerScore) {
+        return alert(`You Lose! ${playerScore} to ${computerScore}`);
+    } else {
+        return alert(`Draw! ${playerScore} to ${computerScore}`);
+    }
+}
 
     
     
 
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(game());
